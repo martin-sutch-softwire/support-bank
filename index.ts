d2ce8931243b01635a185f8c44698285041102d4
martin-sutch-softwire <martin.sutch@softwire.com>
2025-06-19 13:48:21 +0100

    Account Objects and Transaction Objects created from csv

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-const readlineSync = require('readline-sync')
 const fs = require('fs')
 const path = require('path')
 
 const filePath = path.join(__dirname, '../Transactions2014.csv')
 const csvData = fs.readFileSync(filePath, 'utf-8')
-const lines = csvData.split('\n')
+const lines = csvData.trim().split('\r\n')
 const [headerLine, ...dataLines] = lines
 
 class Account {
@@ -12,45 +11,61 @@ class Account {
     funds: number
     constructor (name : string, funds: number) {
         this.name = name
-        this.funds = funds
+        this.funds = funds * 100
+    }
+    deposit(amount: number) {
+        this.funds = this.funds + amount * 100
+    }
+    withdraw(amount: number) {
+        this.funds = this.funds - amount * 100
     }
 }
 
+class Transaction {
+    date: string
+    from: Account
+    to: Account
+    narrative: string
+    amount: number
+    constructor (date: string, from: Account, to: Account, narrative: string, amount: number) {
+        this.date = date
+        this.from = from
+        this.to = to
+        this.narrative = narrative
+        this.amount = amount * 100
+    }
+}
+const accounts: Account[] = []
+const transations: Transaction[] = []
+
+const linkAccount = (accountName: string) => {
+const isNewAccount = !accounts.some(account => account.name === accountName)
+    if (isNewAccount) {
+        const newAccount = new Account(accountName, 0)
+        accounts.push(newAccount)
+        return newAccount
+    }
+    return accounts.find(account => account.name === accountName)
+}
+
 dataLines.forEach((transaction: string) => {
-    
-    console.log(transaction)
+    // 1: Process Accounts
+    const transactionData = transaction.split(',')
+    const accountNameFrom = transactionData[1]
+    const accountFrom = linkAccount(accountNameFrom)
+    const accountNameTo = transactionData[2]
+    const accountTo = linkAccount(accountNameTo)
+
+    // 2: Process Transactions
+    const transactionDate = transactionData[0]
+    const transactionNarrative = transactionData[3]
+    const transactionAmount = Number(transactionData[4])
+    if (accountTo && accountFrom) {
+        const newTransaction = new Transaction(transactionDate,accountFrom,accountTo,transactionNarrative,transactionAmount)
+        transations.push(newTransaction)
+        accountFrom.withdraw(transactionAmount)
+        accountTo.deposit(transactionAmount)
+    }
 });
 
-// console.log(dataLines)
-
-
-// const userName= readlineSync.question('May I have your name? ')
-// console.log('Hi ' + userName)
-
-
-
-/*
-
-1. Create an account for each person
-2. Create transactions between accounts
-    - Reduce amount in 'from' account
-    - Increase amount in 'to' account
-
-
-Account Class
-    - name, funds
-    - function: check name exists
-    - function: add name (and £0 funds)
-    - function: provide name with funds for list all
-
-Transaction Class
-    - date, from, to, narrative, amount
-    - function: provide transaction details for list acccount
-
-Commands:
-List All : 
-    output each name with funds
-List [Account] :
-    output each transaction amount for account with date and narrative
-
-*/
+console.log(accounts)
